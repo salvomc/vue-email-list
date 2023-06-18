b6418e5c819519dae7e378545b0eb031c8c4d50c
salvomc <smcrwrk@gmail.com>
2023-06-18 22:21:05 +0200

    bonus done

---
+++ bonus/js/script.js
@@ -0,0 +1,27 @@
+const {createApp} = Vue;
+
+createApp({
+data(){
+    return {
+                randomMailArray: [],
+           }
+      },
+
+methods:
+{
+    generateRandomMail()
+        {
+            for(let i=0; i<10; i++)
+                {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((mail)=>
+                    {
+                    this.randomMailArray.push(mail.data.response)
+                    })      
+                }
+        }
+},
+mounted()
+    {
+        this.generateRandomMail()
+    }
+}).mount('#app')
